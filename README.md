a88de7138771f4d8ecd45fc89aee296582be48cd
gypark <gypark@charzin.com>
2022-06-19 22:01:12 +0900

    Readme 추가

---
+++ README.md
@@ -0,0 +1,40 @@
+# Composer Architecture
+
+## 설치 방법
+
+OS X & 리눅스:
+
+```sh
+terraform init
+terraform plan
+terraform apply
+```
+
+윈도우:
+
+```sh
+terraform init
+terraform plan
+terraform apply
+```
+
+## 사용 예제
+
+_더 많은 예제와 사용법은 [Wiki][wiki]를 참고하세요._
+
+## 개발 환경 설정
+
+## 업데이트 내역
+
+- 0.0.1
+  - 작업 진행 중
+
+## 정보
+
+박경열 – a1061602@gmail.com
+
+MIT 라이센스를 준수하며 `LICENSE`에서 자세한 정보를 확인할 수 있습니다.
+
+[https://github.com/ComposerLab/composer-architecture/blob/master/License](https://github.com/ComposerLab/composer-architecture/blob/master/License)
+
+[wiki]: https://github.com/ComposerLab/composer-architecture/wiki
---
+++ setup-automation/git-action-composer-websetup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+gh secret set AWS_ACCESS_KEY_ID -b $(terraform output -raw publisher_access_key) -o ComposerLab -r composer-web
+gh secret set AWS_SECRET_ACCESS_KEY -b $(terraform output -raw publisher_secret_key) -o ComposerLab -r composer-web
+gh secret set AWS_REGION -b $(terraform output -raw aws_region) -o ComposerLab -r composer-web
+gh secret set ECR_REPOSITORY_NAME -b $(terraform output -raw ecr_repository_name) -o ComposerLab -r composer-web
+gh secret set ECS_CLUSTER -b $(terraform output -raw ecs_cluster) -o ComposerLab -r composer-web
+gh secret set ECS_SERVICE -b $(terraform output -raw ecs_service) -o ComposerLab -r composer-web
